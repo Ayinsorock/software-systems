4ebbc9ed363c2bc63e5b7923f45e17f10d2ecb85
Gerjan Wielink <gerjan.wielink@nedap.com>
2018-11-21 00:04:45 +0100

    QUESTS!

--- ss/week2/ThreeWayLamp.java
+++ ss/week2/ThreeWayLamp.java
@@ -6,24 +6,25 @@ package ss.week2;
 public class ThreeWayLamp {
 	private LampSetting setting;
 	/**
-	 * @ensure this.setting == LampSetting.OFF
+	 * @ ensure this.setting == LampSetting.OFF
 	 */
 	public ThreeWayLamp() {
 		this.setting = LampSetting.OFF;
 	}
 
 	/**
-	 * @ensure \result == this.setting
+	 * @ pure
+	 * @ ensure \result == this.setting
 	 */
 	public LampSetting currentSetting () {
 		return this.setting;
 	}
 
 	/**
-	 * @ensure \old(currentSetting() == LampSetting.OFF ==> currentSetting() == LampSetting.LOW;
-	 * 		   \old(currentSetting() == LampSetting.LOW ==> currentSetting() == LampSetting.MEDIUM;
- 	 *         \old(currentSetting() == LampSetting.MEDIUM ==> currentSetting() == LampSetting.HIGH;
-	 *         \old(currentSetting() == LampSetting.HIGH ==> currentSetting() == LampSetting.OFF;
+	 * @ ensure \old(currentSetting() == LampSetting.OFF ==> currentSetting() == LampSetting.LOW;
+	 * 	        \old(currentSetting() == LampSetting.LOW ==> currentSetting() == LampSetting.MEDIUM;
+ 	 *          \old(currentSetting() == LampSetting.MEDIUM ==> currentSetting() == LampSetting.HIGH;
+	 *          \old(currentSetting() == LampSetting.HIGH ==> currentSetting() == LampSetting.OFF;
 	 */
 	public void  switchSetting () {
 		switch (this.setting) {
